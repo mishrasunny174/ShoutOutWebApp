41f274b2377e3c493185a66c9d8bd06e81e3572e
sunny mishra <mishrasunny174@gmail.com>
2018-07-07 14:46:23 +0530

    added basic layout

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Shout it!</title>
+    <link rel='stylesheet' href='css/style.css' type='text/css'/>
+  </head>
+  <body>
+    <div id='shoutbox'>
+      <header>
+        <h1>Shout-Out Box</h1>
+      </header>
+      <div id='shouts'>
+        <ul>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+          <li class='shout'><span id='time_span'>13:12 : </span>Sunny - Fuck the world</li>
+        </ul>
+      </div>
+      <div id='input_box'>
+        <form method="post" action="process.php">
+          <input type='text' name='name' placeholder="Enter your name"/>
+          <input type='text' name='message' placeholder="Enter your message"/>
+          <br>
+          <input type='submit' name='submit' value='Shout it out' class='shout_out_button'/>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
